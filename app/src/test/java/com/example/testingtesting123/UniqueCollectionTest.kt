dc744a82e700fe2f4735f9417f17939f0ffa3997
dominiquebissey <dominiquebissey04@gmail.com>
2025-04-15 11:50:20 -0400

    Added two additional tests for adding item and item uniqueness. Added function to clear collection of items.

--- app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
+++ app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
@@ -16,14 +16,21 @@ class UniqueCollectionTest {
     // TODO 1: Write a test to ensure items can be added to the collection
     @Test
     fun addAnItem() {
+        collection.addItem(Item("item1"))
+        val item = collection.get(0)
 
+        assert(item.name == "item1")
     }
 
     // TODO 2: Write a test to ensure that only unique items can be added to the collection
     // Uniqueness is determined by the Item.name property, which is set via the constructor
     @Test
     fun addUniqueItem() {
+        collection.addItem(Item("item1"))
+        collection.addItem(Item("item1"))
+        collection.addItem(Item("item2"))
 
+        assert(collection.size() == 2)
     }
 
     // Test Driven Development (TDD) test - complete specified function so that this test passes
